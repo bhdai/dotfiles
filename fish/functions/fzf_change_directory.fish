47c2ef4a67e1f4c3fb568a8818189f046fe0fae7
Dailuong123 <ss1280dzzz@gmail.com>
2024-07-25 22:09:02 +0700

    refactor(fish): add theme and plugins i'm using
    add tokyo theme, and add adjust the fzf_change_directory to exclude the ANSI color codes

---
+++ fish/functions/fzf_change_directory.fish
@@ -0,0 +1,27 @@
+function _fzf_change_directory
+  fzf | perl -pe 's/([ ()])/\\\\$1/g'|read foo
+  if [ $foo ]
+    builtin cd $foo
+    commandline -r ''
+    commandline -f repaint
+  else
+    commandline ''
+  end
+end
+
+function fzf_change_directory
+  begin
+    # Define a sed pattern to remove ANSI color codes
+    set -l remove_ansi '\x1B\[[0-9;]*[mK]'
+    
+    echo $HOME/.config
+
+    find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+
+    ls --color=never -ad */ | sed -r "s/$remove_ansi//g" | perl -pe "s#^#$PWD/#" | grep -v \.git 
+
+    ls --color=never -d $HOME/clones/*/ | sed -r "s/$remove_ansi//g" | grep -v \.git 
+
+    ls --color=never -d $HOME/workplace/*/ | sed -r "s/$remove_ansi//g" | grep -v \.git 
+  end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+end
