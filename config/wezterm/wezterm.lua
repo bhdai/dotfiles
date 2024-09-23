258f3db305c420f12cbb57b8a6fba480d87427cb
buidai123 <ss1280dzzz@gmail.com>
2024-09-23 22:19:37 +0700

    chore: update

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,12 +15,36 @@ config.webgpu_power_preference = "HighPerformance"
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 
+-- Colorscheme
+config.color_scheme_dirs = { wezterm.home_dir .. "/ghq/github.com/folke/tokyonight.nvim/extras/wezterm/" }
+config.color_scheme = "tokyonight_storm"
+wezterm.add_to_config_reload_watch_list(config.color_scheme_dirs[1] .. config.color_scheme .. ".toml")
+
 config.colors = {
 	indexed = { [241] = "#65bcff" },
 }
 
--- Theme
-config.color_scheme = "Tokyo Night Storm"
+-- config.underline_thickness = 3
+config.cursor_thickness = 3
+-- config.underline_position = -6
+
+-- for windows
+if wezterm.target_triple:find("windows") then
+	config.default_prog = { "pwsh" }
+	config.window_decorations = "RESIZE|TITLE"
+	wezterm.on("gui-startup", function(cmd)
+		local screen = wezterm.gui.screens().active
+		local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+		local gui = window:gui_window()
+		local width = 0.7 * screen.width
+		local height = 0.7 * screen.height
+		gui:set_inner_size(width, height)
+		gui:set_position((screen.width - width) / 2, (screen.height - height) / 2)
+	end)
+else
+	config.term = "wezterm"
+	config.window_decorations = "NONE"
+end
 
 -- Fonts
 config.font_size = 13
@@ -29,7 +53,7 @@ config.bold_brightens_ansi_colors = true
 
 -- Cursor
 config.default_cursor_style = "BlinkingBar"
-config.force_reverse_video_cursor = true
+config.force_reverse_video_cursor = false
 config.window_padding = { left = 10, right = 0, top = 10, bottom = 4 }
 config.window_background_opacity = 1
 -- cell_width = 0.9,
