eb3ed79256e98ca0ccb90f6ecad2379909b2905e
Bui Huu Dai <ss1280dzzz@gmail.com>
2024-12-09 12:31:49 +0700

    feat(fish): add zrok auto completion

--- config/fish/functions/pytime.fish
+++
@@ -1,8 +0,0 @@
-# run python script with timing
-function pytime
-  set start (date +%s.%N)
-  python $argv
-  set end (date +%s.%N)
-  set runtime (echo "$end - $start" | bc -l)
-  printf "Runtime: %.6f seconds\n" $runtime
-end
