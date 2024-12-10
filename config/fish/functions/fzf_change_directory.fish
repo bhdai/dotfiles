7a34a60fbe99de67bf614d73c09bfde949a5904c
Bui Huu Dai <ss1280dzzz@gmail.com>
2024-12-10 22:25:20 +0700

    feat(fish): improve fzf change directory function
    expand to find all the possible files or directory and subdirectoies in
    the current directory
    
    and check if it a file open it with nvim otherwise jump in to it

--- config/fish/functions/fzf_change_directory.fish
+++ config/fish/functions/fzf_change_directory.fish
@@ -1,28 +1,35 @@
-function _fzf_change_directory
-  fzf | perl -pe 's/([ ()])/\\\\$1/g' | read foo
-  if test -n "$foo"
-    builtin cd $foo
-    commandline -r ''
-    commandline -f repaint
-  else
-    commandline ''
-  end
+function _fzf_open_or_cd
+    fzf | perl -pe 's/([ ()])/\\\\$1/g' | read -l result
+    if test -n "$result"
+        if test -f "$result"
+            # if it's a file open it with neovim
+            nvim "$result"
+        else if test -d "$result"
+            # if it a directory cd to it
+            builtin cd "$result"
+            commandline -r ''
+            commandline -f repaint
+        end
+    else
+        commandline ''
+    end
 end
 
 function fzf_change_directory
-  begin
-    # Define a sed pattern to remove ANSI color codes
-    set -l remove_ansi '\x1B\[[0-9;]*[mK]'
+    begin
+        echo $HOME/.config
 
-    echo $HOME/.config
+        find (ghq root) -maxdepth 4 \( -type d -name .git -prune \) -o \( -type f -o -type d \) | sed 's/\/\.git//'
 
-    find (ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+        # use find to list both files and directories in current directory and subdirectories
+        find . \( -type f -o -type d \) | sed -r "s#^\./##" | perl -pe "s#^#$PWD/#" | grep -v '\.git'
 
-    # Use find instead of ls to list directories
-    find . -maxdepth 1 -type d | sed -r "s#^\./##" | sed -r "s/$remove_ansi//g" | perl -pe "s#^#$PWD/#" | grep -v \.git
+        find $HOME/Downloads -maxdepth 1 \( -type f -o -type d \) | grep -v '\.git'
 
-    find $HOME/Downloads -maxdepth 1 -type d | sed -r "s/$remove_ansi//g" | grep -v \.git
+        find $HOME/.config -maxdepth 1 \( -type f -o -type d \) | grep -v '\.git'
 
-    find $HOME/workplace -maxdepth 2 -type d | sed -r "s/$remove_ansi//g" | grep -v \.git
-  end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+        find $HOME/ghq/github.com/buidai123/dotfiles/config/ -maxdepth 1 \( -type f -o -type d \) | grep -v '\.git'
+
+        find $HOME/workplace -maxdepth 2 \( -type f -o -type d \) | grep -v '\.git'
+    end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_open_or_cd $argv
 end
