9e376391d01fdae89e583da9c25f9cd658387404
Bui Dai <ss1280dzzz@gmail.com>
2025-04-27 19:10:25 +0700

    chore(fish): add spark paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,6 +27,8 @@ fish_add_path /bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path /usr/local/sbin
+fish_add_path /opt/apache-spark/bin/
+fish_add_path /opt/apache-spark/sbin/
 
 # Go
 set -x GOPATH ~/go
