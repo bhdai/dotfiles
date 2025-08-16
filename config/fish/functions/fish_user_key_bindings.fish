ea56125d7ab81fe2446fad68626b01358e51dcb1
Bui Dai <ss1280dzzz@gmail.com>
2025-08-16 11:19:35 +0700

    fix(fish): paste using p in normal mode should now work as in vi

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,9 @@ function fish_user_key_bindings
   fish_default_key_bindings -M insert
   fish_vi_key_bindings --no-erase insert
 
+
+  set -g fish_cursor_end_mode exclusive
+
   # fzf
   bind \cf fzf_change_directory
   #bind -M insert \ch __fzf_tldr
@@ -11,5 +14,6 @@ function fish_user_key_bindings
 
   bind -M visual -m default y 'fish_clipboard_copy; commandline -f end-selection repaint-mode'
   bind yy fish_clipboard_copy
-  bind p fish_clipboard_paste
+  # bind p fish_clipboard_paste
+  bind p 'set -g fish_cursor_end_mode exclusive' forward-char 'set -g fish_cursor_end_mode inclusive' fish_clipboard_paste
 end
